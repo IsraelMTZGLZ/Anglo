a46b90231c3b322d5a2116522df91be4c087176b
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-01-26 13:17:01 -0600

    list

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,10 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends MY_RootController {
 
 	public function index()
 	{
+		$this->_initialPage();
 		$this->load->view('home_view');
+		$this->_footerPage();
 	}
+
 }
