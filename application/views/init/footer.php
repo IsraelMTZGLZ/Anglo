e8541a38a553021afc3d1c6f1b95eed73b430828
hectorium110996 <hectori.um.15@gmail.com>
2020-01-26 11:57:15 -0600

    init changes

---
+++ application/views/init/footer.php
@@ -0,0 +1,96 @@
+<footer class="footer-area">
+        <div class="footer-top section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-3">
+                        <div class="footer-text">
+                            <h4 class="upper">Classic</h4>
+                            <p>If you are going to use a passage of Lorem Ipsum, you need to be sure</p>
+                            <div class="social-menu">
+                                <a href="#"><i class="icofont icofont-social-facebook"></i></a>
+                                <a href="#"><i class="icofont icofont-social-twitter"></i></a>
+                                <a href="#"><i class="icofont icofont-social-google-plus"></i></a>
+                                <a href="#"><i class="icofont icofont-social-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-md-2 col-md-offset-1">
+                        <div class="footer-single">
+                            <h4 class="upper">News</h4>
+                            <ul>
+                                <li><a href="#">Subsciption</a></li>
+                                <li><a href="#">New Apps</a></li>
+                                <li><a href="#">Download now</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-md-2">
+                        <div class="footer-single">
+                            <h4 class="upper">Company</h4>
+                            <ul>
+                                <li><a href="#">Screenshot</a></li>
+                                <li><a href="#">Fetures</a></li>
+                                <li><a href="#">Price</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-md-2">
+                        <div class="footer-single">
+                            <h4 class="upper">Resources</h4>
+                            <ul>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Privacy &amp; Term</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-md-2">
+                        <div class="footer-single">
+                            <h4 class="upper">Solutions</h4>
+                            <ul>
+                                <li><a href="#">Bug Fixing</a></li>
+                                <li><a href="#">Upgrade</a></li>
+                                <li><a href="#">Malware Protect</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <p class="copyright">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icofont icofont-heart-alt" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer-Area / -->
+
+
+    <!--Vendor-JS-->
+    <script src="<?=base_url('resources/assets/js/vendor/jquery-1.12.4.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/vendor/bootstrap.min.js');?>"></script>
+    <!--Plugin-JS-->
+    <script src="<?=base_url('resources/assets/js/owl.carousel.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/appear.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/bars.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/waypoints.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/counterup.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/easypiechart.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/mixitup.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/contact-form.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/scrollUp.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/magnific-popup.min.js');?>"></script>
+    <script src="<?=base_url('resources/assets/js/wow.min.js');?>"></script>
+    <!--Main-active-JS-->
+    <script src="<?=base_url('resources/assets/js/main.js');?>"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXZ3vJtdK6aKAEWBovZFe4YKj1SGo9V20&callback=initMap"></script>
+    <script src="<?=base_url('resources/assets/js/maps.js');?>"></script>
+</body>
+
+</html>    
---
+++ application/views/init/initial.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="John Doe">
+    <meta name="description" content="">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Classic One Page Template</title>
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="shortcut icon" type="image/ico" href="images/favicon.ico" />
+    <!-- Plugin-CSS -->
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/bootstrap.min.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/owl.carousel.min.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/icofont.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/magnific-popup.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/animate.css');?>">
+    <!-- Main-Stylesheets -->
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/normalize.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/style.css');?>">
+    <link rel="stylesheet" href="<?=base_url('resources/assets/css/responsive.css');?>">
+    <script src="<?=base_url('resources/assets/js/vendor/modernizr-2.8.3.min.js');?>"></script>
+
+    <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
