d2bda87263c0275d7d50839c54bbe1f4b681751d
hectorium110996 <hectori.um.15@gmail.com>
2020-01-26 13:08:12 -0600

    font awesome added

--- application/views/init/initial.php
+++ application/views/init/initial.php
@@ -25,8 +25,68 @@
     <link rel="stylesheet" href="<?=base_url('resources/assets/css/responsive.css');?>">
     <script src="<?=base_url('resources/assets/js/vendor/modernizr-2.8.3.min.js');?>"></script>
 
+	<link rel="stylesheet" href="<?=base_url('resources/assets/fontawesome/css/all.min.css');?>">
+	<style>
+	.social-bar {
+		position: fixed;
+		right: 0;
+		top: 35%;
+		font-size: 1.5rem;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		z-index: 100;
+	}
+
+	.icon {
+		color: white;
+		text-decoration: none;
+		padding: .7rem;
+		display: flex;
+		transition: all .5s;
+	}
+
+	.icon-facebook {
+		background: #2E406E;
+	}
+
+	.icon-twitter {
+		background: #339DC5;
+	}
+
+	.icon-youtube {
+		background: #E83028;
+	}
+
+	.icon-instagram {
+		background: #3CDA5B;
+	}
+
+	.icon:first-child {
+		border-radius: 1rem 0 0 0;
+	}
+
+	.icon:last-child {
+		border-radius: 0 0 0 1rem;
+	}
+
+	.icon:hover {
+		padding-right: 3rem;
+		border-radius: 1rem 0 0 1rem;
+		box-shadow: 0 0 .5rem rgba(0, 0, 0, 0.42);
+	}
+	</style>
     <!--[if lt IE 9]>
         <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+<body>
+
+<!--Boton redes sociales-->
+<div class="social-bar">
+    <a href="#" class="icon icon-facebook" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="#" class="icon icon-twitter" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="#" class="icon icon-youtube" target="_blank"><i class="fab fa-youtube"></i></a>
+    <a href="#" class="icon icon-instagram" target="_blank"><i class="fab fa-whatsapp"></i></a>
+  </div>
